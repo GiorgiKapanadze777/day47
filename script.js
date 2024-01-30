fa184d8bfe755d86efc88b8102f8436481559726
GiorgiKapanadze777 <129076743+GiorgiKapanadze777@users.noreply.github.com>
2024-01-30 20:23:13 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let inputEmail = document.getElementById('inputEmail')
+let inputName = document.getElementById('inputName')
+let inputPassword = document.getElementById('inputPassword')
+
+let inputBtn = document.getElementById('inputBtn')
+
+inputBtn.addEventListener('click', function(){
+    console.log(inputEmail.value, inputName.value, inputPassword.value)
+})
+
+function gio(){
+    prompt('Hello user, Enter your name: ')
+}
+
+gio()
+
+// function myFunction() {
+//     alert("gamarjoba")
+// }
+    // function counter(name) {
+    //     console.log(`${name} gamarjoba`)
+    // };
+    // alert('gamarjoba')
+    // counter('davit') 
+
+
+    // let name = prompt("Please enter your name: ");
+    // console.log(name)
+
+    // let input3 = document.getElementById("t2");
+    // console.log(input3.value);
+
+    // console.log(10 / 2);
+    // alert("360" - "360");
